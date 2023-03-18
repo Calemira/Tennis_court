c1c67998bc5a4caccf7299238f4a7e2399b105c9
Calemira <wercik0908@gmail.com>
2023-03-18 21:52:42 +0100

    cancelling reservation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta
+
+
 # today = date.today()
 
 
@@ -19,7 +21,7 @@ class Menu:
             case 1:
                 reservation.making_reservation()
             case 2:
-                print('Cancel')
+                reservation.cancel_reservation()
             case 3:
                 reservation.print_schedule()
             case 4:
@@ -29,9 +31,9 @@ class Menu:
 
 
 class Reservations:
-
     now = datetime.now()
     list_of_reservations = []
+    menu = Menu()
 
     def making_reservation(self):
         i = 0
@@ -40,31 +42,36 @@ class Reservations:
             date = input('When would you like to make a reservation {DD.MM.YYY HH:MM}\n')
             date_format = '%d.%m.%Y %H:%M'
             date = datetime.strptime(date, date_format)
-            self.check_if_possible(date)
-            duration = input('How long would you like to book?\na) 30 minutes\nb) 60 minutes\nc) 90 minutes\n')
-            match duration:
-                case 'a':
-                    duration = 30
-                case 'b':
-                    duration = 60
-                case 'c':
-                    duration = 90
-                case _:
-                    print('you chose nothing')
-            self.check_if_available(date, duration)
-            self.stored_reservations(full_name, date, duration)
-            answer_1 = int(input('Reservation completed. What would you like to do now?\n1.Make another reservation\n2.Go to the main menu\n3.Exit'))
-            if answer_1 == 1:
-                pass
-            if answer_1 == 2:
+            is_possible = self.check_if_possible(date)
+            if is_possible:
+                duration = input('How long would you like to book?\na) 30 minutes\nb) 60 minutes\nc) 90 minutes\n')
+                match duration:
+                    case 'a':
+                        duration = 30
+                    case 'b':
+                        duration = 60
+                    case 'c':
+                        duration = 90
+                    case _:
+                        print('you chose nothing')
+                is_available = self.check_if_available(date, duration)
+            elif not is_possible:
+                print('The date you have choosen is either in the past or in less than two hours.')
+            if is_available:
+                self.stored_reservations(full_name, date, duration)
+                answer_1 = int(input(
+                    'Reservation completed. What would you like to do now?\n1.Make another reservation\n2.Go to the main menu\n3.Exit'))
+                if answer_1 == 1:
+                    pass
+                if answer_1 == 2:
+                    i = 1
+                    self.open_menu()
+                if answer_1 == 3:
+                    i = 1
+                    exit()
+            elif not is_available:
+                print('The date you choose is already taken.')
                 i = 1
-                menu = Menu()
-                menu.display_menu()
-                answer = int(input())
-                menu.match_answer(answer)
-            if answer_1 == 3:
-                i =1
-                exit
 
     def stored_reservations(self, full_name, date, duration):
         date = date.strftime('%d.%m.%Y %H:%M')
@@ -76,40 +83,51 @@ class Reservations:
             print(i)
 
     def check_if_possible(self, ext_day):
-        i = 0
-        while i == 0:
+        while True:
             if Reservations.now + timedelta(hours=2) <= ext_day:
-                i = 1
                 return True
             else:
-                print('The time you choose is in the past or is in less than two hours. please choose a different time\n')
+                return False
 
     def check_if_available(self, starting_time, duration):
-        while True:
-            if len(Reservations.list_of_reservations) == 0:
-                return True
+        if len(Reservations.list_of_reservations) == 0:
+            return True
+        else:
+            ending_time = starting_time + timedelta(minutes=int(duration))
+            for i in Reservations.list_of_reservations:
+                i_start = datetime.strptime(i['date'], '%d.%m.%Y %H:%M')
+                i_end = i_start + timedelta(minutes=i['duration'])
+                if i_end > starting_time and ending_time > i_start:
+                    return False
+                else:
+                    return True
+
+    def cancel_reservation(self):
+        b = False
+        full_name = input('Please provide the name that you have made the reservation with\n')
+        for i in Reservations.list_of_reservations:
+            if i.get('full name') == full_name:
+                date = input('Please provide the date with and hour that you have booked {DD.MM.YYY HH:MM}\n')
+                if i.get('date') == date:
+                    Reservations.list_of_reservations.remove(i)
+                    b = True
+                else:
+                    print('You have no reservation in that time\n')
             else:
-                ending_time = starting_time + timedelta(minutes=int(duration))
-                for i in Reservations.list_of_reservations:
-                    i_start = datetime.strptime(i['date'], '%d.%m.%Y %H:%M')
-                    i_end = i_start + timedelta(minutes=i['duration'])
-                    if i_end > starting_time and ending_time > i_start:
-                        print('The time you have picked is taken. Please choose a different time\n')
-                        return False
-                return True
-
-
-    def cancel_reservation(self, full_name, date):
-        # here i have to search trough my dict to look for persons reservation having provided their
-        # name and date and delete it
-        # i also have to check if the reservation is in less then an hour or if it even exists,
-        # if not cancel the process
-        pass
+                print('You dont have any reservations under your name\n')
+                self.open_menu()
+        if b:
+            print('We succesfully cancelled your reservation')
+            self.open_menu()
 
     def save_to_file(self):
         # save dict of reservations to file
         pass
 
+    def open_menu(self):
+        menu.display_menu()
+        answer = int(input())
+        menu.match_answer(answer)
 
 menu = Menu()
 menu.display_menu()
